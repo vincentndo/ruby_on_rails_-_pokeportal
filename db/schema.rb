da2286e858d641379fd8550b5b3472baeebd0ac8
Ethan Lee <hello@ethanl.ee>
2018-10-15 14:52:09 -0700

    Undo migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181015180340) do
-
-  create_table "pokemons", force: :cascade do |t|
-    t.string "name"
-    t.integer "level"
-    t.integer "trainer_id"
-    t.integer "ndex"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["trainer_id"], name: "index_pokemons_on_trainer_id"
-  end
+ActiveRecord::Schema.define(version: 2017_10_11_232615) do
 
   create_table "trainers", force: :cascade do |t|
     t.string "name"
